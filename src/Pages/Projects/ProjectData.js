1d8204e70eb5aee35515c2ec9063daa1c3f8f8db
Kazeem Erinfolami <erinfolamibolaji2010@gmail.com>
2021-05-05 13:20:11 +0100

    added some pics

--- src/Pages/Projects/ProjectData.js
+++ src/Pages/Projects/ProjectData.js
@@ -1,22 +1,31 @@
-import p11 from "../../img/projPic/p1.1.png"
-import p12 from "../../img/projPic/p1.2.png"
-import p13 from "../../img/projPic/p1.3.png"
-import p14 from "../../img/projPic/p1.4.png"
-import p15 from "../../img/projPic/p1.5.png"
-import p16 from "../../img/projPic/p1.6.png"
-import p17 from "../../img/projPic/p1.7.png"
-import p21 from "../../img/projPic/p2.1.png"
-import p22 from "../../img/projPic/p2.2.png"
-import p23 from "../../img/projPic/p2.3.png"
-import p31 from "../../img/projPic/p3.1.png"
-import p32 from "../../img/projPic/p3.2.png"
-import p33 from "../../img/projPic/p3.3.png"
-import p34 from "../../img/projPic/p3.4.png"
-import p35 from "../../img/projPic/p3.5.png"
-import p36 from "../../img/projPic/p3.6.png"
-import p37 from "../../img/projPic/p3.7.png"
-import p41 from "../../img/projPic/p4.1.png"
-import p42 from "../../img/projPic/p4.2.png"
+import p11 from "../../img/projPic/p1.1.png";
+import p12 from "../../img/projPic/p1.2.png";
+import p13 from "../../img/projPic/p1.3.png";
+import p14 from "../../img/projPic/p1.4.png";
+import p15 from "../../img/projPic/p1.5.png";
+import p16 from "../../img/projPic/p1.6.png";
+import p17 from "../../img/projPic/p1.7.png";
+import p21 from "../../img/projPic/p2.1.png";
+import p22 from "../../img/projPic/p2.2.png";
+import p23 from "../../img/projPic/p2.3.png";
+import p31 from "../../img/projPic/p3.1.png";
+import p32 from "../../img/projPic/p3.2.png";
+import p33 from "../../img/projPic/p3.3.png";
+import p34 from "../../img/projPic/p3.4.png";
+import p35 from "../../img/projPic/p3.5.png";
+import p36 from "../../img/projPic/p3.6.png";
+import p37 from "../../img/projPic/p3.7.png";
+import p41 from "../../img/projPic/p4.1.png";
+import p42 from "../../img/projPic/p4.2.png";
+import p43 from "../../img/projPic/p5.1.png";
+import p44 from "../../img/projPic/p5.2.png";
+import p45 from "../../img/projPic/p5.3.png";
+import p46 from "../../img/projPic/p5.4.png";
+import p47 from "../../img/projPic/p5.5.png";
+import p48 from "../../img/projPic/p5.6.png";
+import p49 from "../../img/projPic/p5.7.png";
+import p50 from "../../img/projPic/p5.8.png";
+
 
 
 export const ProjectData = [
@@ -45,7 +54,7 @@ export const ProjectData = [
     }, {
         projIMG: p17,
         url: "http://iqstore.ivyarc.com/"
-    }]
+    }];
 export const ProjectData2 = [
     {
         projIMG: p21,
@@ -86,7 +95,7 @@ export const ProjectData3 = [
     {
         projIMG: p37,
         url: "https://kind-agnesi-86dcc5.netlify.app/"
-    }]
+    }];
 export const ProjectData4 = [
     {
         projIMG: p41,
@@ -95,4 +104,39 @@ export const ProjectData4 = [
     {
         projIMG: p42,
         url: "https://brave-beaver-b75543.netlify.app/"
-    }]
+    }];
+export const ProjectData5 = [
+    {
+        projIMG: p43,
+        url: "https://dev-ieducare.turontechnologies.com/mydashboard"
+    },
+    {
+        projIMG: p44,
+        url: "https://dev-ieducare.turontechnologies.com/mydashboard"
+    },
+    {
+        projIMG: p45,
+        url: "https://dev-ieducare.turontechnologies.com/mydashboard"
+    },
+    {
+        projIMG: p46,
+        url: "https://dev-ieducare.turontechnologies.com/mydashboard"
+    },
+    {
+        projIMG: p47,
+        url: "https://dev-ieducare.turontechnologies.com/mydashboard"
+    },
+    {
+        projIMG: p48,
+        url: "https://dev-ieducare.turontechnologies.com/mydashboard"
+    }
+    ,
+    {
+        projIMG: p49,
+        url: "https://dev-ieducare.turontechnologies.com/mydashboard"
+    }
+    ,
+    {
+        projIMG: p50,
+        url: "https://dev-ieducare.turontechnologies.com/mydashboard"
+    }];
--- src/Pages/Projects/ProjectIMG.js
+++ src/Pages/Projects/ProjectIMG.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import { motion } from "framer-motion"
 
-import { ProjectData, ProjectData2, ProjectData3, ProjectData4 } from './ProjectData'
+import { ProjectData, ProjectData2, ProjectData3, ProjectData4, ProjectData5 } from './ProjectData'
 
 function ProjectIMG() {
     return (
         <div>
             <div className="project-img-wrapper">
+                <div>
+                    <div>Project: LMS "e-Learning platform"</div>
+                    <div
+                        className="row center">
+                        {ProjectData5.map((D) => (
+                            <div
+                                className="card" key={D.projIMG}>
+                                <motion.a
+                                    whileHover={{
+                                        scale: 1.3,
+                                        textShadow: "0px 0px 8px #333333",
+                                    }}
+                                    transition={{
+                                        type: "spring",
+                                        stiffness: 1000,
+                                    }}
+                                    href={D.url} target="_blank" rel="noopener noreferrer">
+                                    <img className="medium" src={D.projIMG} alt="product img" />
+                                </motion.a>
+                            </div>
+                        ))}
+                    </div>
+                </div>
                 <div >
                     <div>Project: LMS "Learning Management System"</div>
                     <div
